e1e7fec45fb5d8a388f003faf0d18f045dedbb1a
Aravind <mailavj@gmail.com>
2018-11-01 16:03:35 +0000

    Chapter 6

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -9,11 +9,14 @@ admin.site.register(Genre)
 #admin.site.register(BookInstance)
 admin.site.register(Language)
 
+class BookInline(admin.TabularInline):
+    model = Book
+
 #Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
+    inlines = [BookInline]
 #Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+def index(request):
+    """View function for homepage of site."""
+
+    #Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    #Available book (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    #The 'all()' is implied by default
+    num_authors = Author.objects.count()
+
+    #Searching for books containing Analog
+    num_analog_books = Book.objects.filter(title__contains='analog').count()
+
+    #Number of fiction Genre
+    num_fiction_genre = Genre.objects.filter(name__contains='fiction').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_analog_books': num_analog_books,
+        'num_fiction_genre': num_fiction_genre,
+    }
+
+    #Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context = context)
+
+class BookListView(generic.ListView):
+    model = Book
