2c7e8222e7a0f65fc9205c56d5be54787efd2080
Aravind <mailavj@gmail.com>
2018-11-01 17:33:31 +0000

    Paginator

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -37,7 +37,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model=Book
